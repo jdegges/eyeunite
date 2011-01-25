1c2ce64f8b0a4ecf421c5bf2aec5464632d79a08
Joey Degges <jdegges@gmail.com>
2011-01-25 07:53:50 -0800

    Added new user request and fixed some bugs in server
      * Get /u => creates new user token
      * Fixed malformed XML response
      * Added port range checking (0 => default)

--- appengine/main.py
+++ appengine/main.py
@@ -21,6 +21,30 @@ class ActivePeers(db.Model):
   lid = db.StringProperty(required=True, indexed=True)
   pid = db.StringProperty(required=True)
 
+
+class NewUser(webapp.RequestHandler):
+  def get(self):
+    self.handle()
+  def post(self):
+    self.handle()
+  def handle(self):
+    port = int(self.request.get('o', default_value='3938'))
+    if port is 0: port = 3938
+    pid = hashlib.sha224(self.request.url + self.request.remote_addr
+                       + str(random.random())).hexdigest()
+    p = Peer(pid=pid, ip=self.request.remote_addr, port=port)
+    p.put()
+
+    self.response.out.write("<?xml version=\"1.0\"?>\n")
+    self.response.out.write("<eyeunite>\n")
+    self.response.out.write("  <peer>\n")
+    self.response.out.write("    <pid>" + p.pid + "</pid>\n")
+    self.response.out.write("    <ip>" + p.ip + "</ip>\n")
+    self.response.out.write("    <port>" + str(p.port) + "</port>\n")
+    self.response.out.write("  </peer>\n")
+    self.response.out.write("</eyeunite>\n")
+
+
 class NewLobby(webapp.RequestHandler):
   def get(self):
     self.handle()
@@ -28,12 +52,18 @@ class NewLobby(webapp.RequestHandler):
     self.handle()
   def handle(self):
     port = int(self.request.get('o', default_value='3938'))
+    if port is 0: port = 3938
     pid = self.request.get('p', default_value='')
     if pid is '':
       pid = hashlib.sha224(self.request.url + self.request.remote_addr
                          + str(random.random())).hexdigest()
       p = Peer(pid=pid, ip=self.request.remote_addr, port=port)
       p.put()
+    else:
+      p = db.GqlQuery("SELECT * FROM Peer WHERE pid = :1", pid).get()
+      if p is None:
+        self.response.out.write("Error: invalid pid")
+        return
 
     lid = hashlib.sha224(self.request.url + self.request.remote_addr
                        + str(random.random())).hexdigest()
@@ -43,7 +73,7 @@ class NewLobby(webapp.RequestHandler):
     ap = ActivePeers(lid=l.lid, pid=p.pid)
     ap.put()
 
-    self.response.out.write("<?xml version=\"1.0\">\n")
+    self.response.out.write("<?xml version=\"1.0\"?>\n")
     self.response.out.write("<eyeunite>\n")
     self.response.out.write("  <lid>" + l.lid + "</lid>\n")
     self.response.out.write("  <peer>\n")
@@ -72,6 +102,7 @@ class JoinLobby(webapp.RequestHandler):
 
     p = None
     port = int(self.request.get('o', default_value='3938'))
+    if port is 0: port = 3938
     pid = self.request.get('p', default_value='')
     if pid is '':
       pid = hashlib.sha224(self.request.url + self.request.remote_addr
@@ -91,7 +122,7 @@ class JoinLobby(webapp.RequestHandler):
       ap = ActivePeers(lid=l.lid, pid=p.pid)
       ap.put()
 
-    self.response.out.write("<?xml version=\"1.0\">\n")
+    self.response.out.write("<?xml version=\"1.0\"?>\n")
     self.response.out.write("<eyeunite>\n")
     self.response.out.write("  <lid>" + l.lid + "</lid>\n")
     self.response.out.write("  <peer>\n")
@@ -113,7 +144,7 @@ class ListLobby(webapp.RequestHandler):
       self.response.out.write("Error: no lid")
       return
 
-    self.response.out.write("<?xml version=\"1.0\">\n")
+    self.response.out.write("<?xml version=\"1.0\"?>\n")
     self.response.out.write("<eyeunite>\n")
     self.response.out.write("  <lid>" + l.lid + "</lid>\n")
 
@@ -133,7 +164,8 @@ class ListLobby(webapp.RequestHandler):
 
 
 application = webapp.WSGIApplication(
-  [ ('/n', NewLobby),
+  [ ('/u', NewUser),
+    ('/n', NewLobby),
     ('/j', JoinLobby),
     ('/l', ListLobby) ],
   debug = True)
