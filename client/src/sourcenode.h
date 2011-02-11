72d06f53b8c755a45b1bcedcade66ea125999c69
COLLIN <collin@ubuntu.(none)>
2011-02-10 19:37:58 -0800

    Modified source initialize, added: receivemessage, sendmessage, and closesocket

--- client/src/sourcenode.h
+++ client/src/sourcenode.h
@@ -1,10 +1,33 @@
 #ifndef SN_SOURCENODE_H
 #define SN_SOURCENODE_
 
-void
-test(void);
+/* TYPES OF MESSAGES THE SOURCE CAN SEND				     */
+typedef enum {
+  FOLLOW_NODE,
+  FEED_NODE,
+  DROP_NODE,
+  REQ_MOVE,
+  REQ_JOIN,
+  REQ_EXIT
+} message_type ;
+
+typedef struct {
+  const char* identity;
+  message_type type;
+} message_struct;
 
 void*
-sn_initzmq (const char* endpoint);
+sn_initzmq (const char* endpoint, const char* pid);
+
+message_struct*
+sn_rcvmsg (void* socket);
+
+int
+sn_closesocket (void* socket);
+
+int
+sn_sendmsg (void* socket, const char* pid, message_type m_type);
+
+
 
 #endif
--- client/tests/hwsource.c
+++ client/tests/hwsource.c
@@ -5,6 +5,5 @@
 
 int main(void)
 {
-  test();
-  return 1;
+  
 }
