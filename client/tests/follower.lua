b754ac8c106551d2a66936f8dedae1d30b490ed9
Joey Degges <jdegges@gmail.com>
2011-02-12 12:35:55 -0800

    Updated source and follower puppets to send/recv real messages.

--- client/tests/follower.lua
+++ client/tests/follower.lua
@@ -1,7 +1,43 @@
 require 'zmq'
 
+function sleep(n)
+  local t0 = os.clock()
+  while os.clock() - t0 <= n do end
+end
 
-function main ()
+-- Send a multipart message to the source like "type|msg"
+function send_request (sock, msg_type, msg)
+  sock:send (msg_type, zmq.SNDMORE)
+  sock:send (msg)
+end
+
+-- Receive a multipart message from the source like "type|msg"
+function recv_request (sock, flags)
+  return sock:recv (flags), sock:recv (flags)
+end
+
+-- Handle the FOLLOW_NODE request
+function follow_node (msg)
+  print ("following \"" .. msg .. "\"")
+end
+
+-- Handle the FEED_NODE request
+function feed_node (msg)
+  print ("feeding \"" .. msg .. "\"")
+end
+
+-- Handle the DROP_NODE request
+function drop_node (msg)
+  print ("dropping \"" .. msg .. "\"")
+end
+
+local request_handler = {
+  FOLLOW_NODE = follow_node,
+  FEED_NODE = feed_node,
+  DROP_NODE = drop_node
+}
+
+function follow ()
   -- Initialize OMQ context
   local ctx = zmq.init (1)
 
@@ -15,11 +51,36 @@ function main ()
   sock:connect ("tcp://127.0.0.1:55555")
 
   -- Send join request to the source
-  sock:send ("JOIN plz")
+  send_request (sock, "REQ_JOIN", "plzzzz")
 
-  -- Wait for source reply
-  msg = sock:recv ()
-  print ("got response: \"" .. msg .. "\"")
+  while true do
+
+    local req_type
+    local req_msg
+
+    -- Non-blocking wait for source message
+    req_type, req_msg = recv_request (sock, zmq.NOBLOCK)
+    if req_type ~= nil and req_msg ~= nil then
+      request_handler[req_type] (req_msg)
+    end
+
+    local rv = math.random ()
+    if rv < 0.85 then
+      -- Do nothing with 85% chance
+      sleep (1)
+    elseif rv < .99 then
+      -- Send move request with 14% chance
+      send_request (sock, "REQ_MOVE", "kthx")
+    else
+      -- Exit with 1% chance
+      send_request (sock, "REQ_EXIT", "bye")
+      break
+    end
+
+  end
+
+  req_type, req_msg = recv_request (sock, 0)
+  request_handler[req_type] (req_msg)
 
   -- Close socket
   sock:close ()
@@ -28,4 +89,6 @@ function main ()
   ctx:term ()
 end
 
-main ()
+math.randomseed (os.time ())
+
+follow ()
--- client/tests/source.lua
+++ client/tests/source.lua
@@ -3,16 +3,38 @@ require 'zmq'
 
 -- Return sender ID and message body
 function recv_from (sock)
-  return sock:recv (), sock:recv ()
+  return sock:recv (), sock:recv (), sock:recv ()
 end
 
 -- Send multipart message into the socket "id | msg"
-function send_to (sock, id, msg)
+function send_to (sock, id, kind, msg)
   sock:send (id, zmq.SNDMORE)
+  sock:send (kind, zmq.SNDMORE)
   sock:send (msg)
 end
 
-function main ()
+-- Handle the REQ_MOVE request
+function req_move (sock, id, msg)
+  send_to (sock, id, "FOLLOW_NODE", "go go go")
+end
+
+-- Handle the REQ_JOIN request
+function req_join (sock, id, msg)
+  send_to (sock, id, "FOLLOW_NODE", "welcome " .. id)
+end
+
+-- Handle the REQ_EXIT request
+function req_exit (sock, id, msg)
+  send_to (sock, id, "DROP_NODE", "good bye " .. id)
+end
+
+local request_handler = {
+  REQ_MOVE = req_move,
+  REQ_JOIN = req_join,
+  REQ_EXIT = req_exit
+}
+
+function source ()
   -- Initialize OMQ context
   local ctx = zmq.init (1)
 
@@ -28,14 +50,16 @@ function main ()
   while true do
 
     local req_id
+    local req_type
     local req_msg
 
     -- Wait for request to come in
-    req_id, req_msg = recv_from (sock)
-    print ("got request: \"" .. req_msg .. "\" from \"" .. req_id .. "\"")
+    req_id, req_type, req_msg = recv_from (sock)
+    print ("got \"" .. req_type .. "\" request: \"" .. req_msg ..
+           "\" from \"" .. req_id .. "\"")
 
-    -- Send out some garbage reply
-    send_to (sock, req_id, "hello?")
+    -- Handle the request
+    request_handler[req_type] (sock, req_id, req_msg)
 
   end
 
@@ -47,4 +71,4 @@ function main ()
 
 end
 
-main ()
+source ()
