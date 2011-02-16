c010aed45c59778ec1d8c73bce94472e17bffec1
Ryan <harris.ucla@gmail.com>
2011-02-16 00:35:50 -0800

    Added simple test for tree. Initial testing works, feel free to begin using tree. Will test more this week.

---
+++ client/tests/tree_test.c
@@ -0,0 +1,61 @@
+#include "tree.h"
+
+
+int main (void){
+
+
+	char pid1[EU_TOKENSTRLEN] = "1";
+	char addr1[EU_ADDRSTRLEN] = "ip1\0";
+
+	char pid2[EU_TOKENSTRLEN] = "2\0";
+	char addr2[EU_ADDRSTRLEN] = "ip2\0";
+
+	char pid3[EU_TOKENSTRLEN] = "3\0";
+	char addr3[EU_ADDRSTRLEN] = "ip3\0";
+
+	char pid4[EU_TOKENSTRLEN] = "4\0";
+	char addr4[EU_ADDRSTRLEN] = "ip4\0";
+
+	char pid5[EU_TOKENSTRLEN] = "5\0";
+	char addr5[EU_ADDRSTRLEN] = "ip5";
+
+	char pid6[EU_TOKENSTRLEN] = "6\0";
+	char addr6[EU_ADDRSTRLEN] = "ip6\0";
+
+	char pid7[EU_TOKENSTRLEN] = "7\0";
+	char addr7[EU_ADDRSTRLEN] = "ip7\0";
+
+	char pid8[EU_TOKENSTRLEN] = "8\0";
+	char addr8[EU_ADDRSTRLEN] = "ip8\0";
+
+	char pid9[EU_TOKENSTRLEN] = "9\0";
+	char addr9[EU_ADDRSTRLEN] = "ip9\0";
+
+	char pid10[EU_TOKENSTRLEN] = "10\0";
+	char addr10[EU_ADDRSTRLEN] = "ip10\0";
+
+	struct tnode* tree = initialize(100, pid1, addr1);
+
+	addpeer(tree, 20, 200, pid2, addr2);
+	addpeer(tree, 30, 300, pid3, addr3);
+	addpeer(tree, 40, 400, pid4, addr4);
+	addpeer(tree, 50, 500, pid5, addr5);
+	addpeer(tree, 50, 500, pid6, addr6);
+	addpeer(tree, 50, 500, pid7, addr7);
+	addpeer(tree, 50, 500, pid8, addr8);
+	addpeer(tree, 50, 500, pid9, addr9);
+	addpeer(tree, 50, 500, pid10, addr10);
+
+	//printTree(tree);
+
+
+	movePeer(tree, pid10);
+
+	//printTree(tree);
+
+
+	removePeer(tree, pid3);
+
+	printTree(tree);
+
+}	
