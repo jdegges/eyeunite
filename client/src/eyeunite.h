caf0e64bf5afe0128daec0dbee14be7f2e5700fe
Collin Lambert <clambert@ucla.edu>
2011-02-24 10:54:54 -0800

    Added packet struct

--- client/src/eyeunite.h
+++ client/src/eyeunite.h
@@ -2,6 +2,7 @@
 #define EU_EYEUNITE_H
 
 #include <netdb.h>
+#include <easyudp.h>
 
 #define EU_ADDRSTRLEN INET6_ADDRSTRLEN
 #define EU_TOKENSTRLEN 224
@@ -12,6 +13,12 @@ struct peer_info
   char addr[EU_ADDRSTRLEN];
   uint16_t port;
   int peerbw;
+};
+
+struct data_pack
+{
+  uint64_t seqnum;
+  char data[EU_PACKETLEN - sizeof (uint64_t)];
 };
 
 #endif
