081be9dab810c8dc419df71a48568eef02991645
Noah <kagan.noah@gmail.com>
2011-03-03 15:26:48 -0800

    Major bug fixes to UDP binding

--- client/src/eyeunitefollower.c
+++ client/src/eyeunitefollower.c
@@ -122,34 +122,39 @@ void drop_downstream_peer(struct peer_node* new_peer)
   return;
 }
 
-void change_upstream_peer(struct peer_info* up_peer)
+void change_upstream_peer(struct peer_info* up_peer, bool bind_eu, int thread_id)
 {
   // Connect to new upstream peer first
   char temp[EU_ADDRSTRLEN*4];
   snprintf (temp, EU_ADDRSTRLEN*4, "tcp://%s:%s", up_peer->addr, up_peer->port);
   void* new_upstream_sock = fn_initzmq (my_pid, temp);
-  void* new_up_eu_sock = eu_socket(EU_PULL);
   struct peer_info my_peer_info;
   memcpy(my_peer_info.pid, my_pid, EU_TOKENSTRLEN);
   memcpy(my_peer_info.addr, my_addr, EU_ADDRSTRLEN);
   memcpy(my_peer_info.port, my_port, EU_PORTSTRLEN);
   my_peer_info.peerbw = my_bw;
   
+  print_error("Sending REQ_JOIN from thread %d", thread_id);
   fn_sendmsg(new_upstream_sock, REQ_JOIN, &my_peer_info);
-  eu_bind(new_up_eu_sock, my_addr, my_port);
   
   // Disconnect any old sockets to previous upstream peer
   if(upstream_sock != NULL)
     fn_closesocket(upstream_sock);
-  if(up_eu_sock != NULL)
-	  eu_close(up_eu_sock);
   
   // Change global variables
   upstream_sock = new_upstream_sock;
-  up_eu_sock = new_up_eu_sock;
 
   if(upstream_peer && upstream_peer != up_peer)
     free(upstream_peer);
+
+  if(bind_eu)
+  {
+    void* new_up_eu_sock = eu_socket(EU_PULL);
+    eu_bind(new_up_eu_sock, my_addr, my_port);
+    if(up_eu_sock != NULL)
+	    eu_close(up_eu_sock);
+    up_eu_sock = new_up_eu_sock;
+  }
 
   upstream_peer = up_peer;
 }
@@ -161,7 +166,7 @@ void* dataThread(void* arg)
   while(1)
   {
     // Blocking recv
-    if((len = eu_recv(up_eu_sock, buf, EU_PACKETLEN, 0, upstream_peer->addr, upstream_peer->port)) > 0)
+    if(up_eu_sock && upstream_peer && (len = eu_recv(up_eu_sock, buf, EU_PACKETLEN, 0, upstream_peer->addr, upstream_peer->port)) > 0)
     {
       struct data_pack* packet = (struct data_pack*)malloc(sizeof(struct data_pack));
       memcpy(packet, buf, len);
@@ -219,7 +224,7 @@ void* statusThread(void* arg)
       print_error("FOLLOW_NODE\n");
       print_error("Changing upstream peer %s\n", pi.pid);
       pthread_mutex_lock(&upstream_peer_mutex);
-      change_upstream_peer(&pi);
+      change_upstream_peer(&pi, true, (int)arg);
       pthread_mutex_unlock(&upstream_peer_mutex);
     }
     else
@@ -341,17 +346,19 @@ int main(int argc, char* argv[])
   print_error ("source ip: %s:%s", upstream_peer->addr, upstream_peer->port);
   
   pthread_mutex_lock(&upstream_peer_mutex);
-  change_upstream_peer(upstream_peer);
+  change_upstream_peer(upstream_peer, false, -1);
   pthread_mutex_unlock(&upstream_peer_mutex);
 
   pthread_t status_thread;
   pthread_t data_thread;
   pthread_t display_thread;
 
+  int thread_id = 0;
+
   // Start status thread
-  pthread_create(&status_thread, NULL, statusThread, NULL);
-  pthread_create(&data_thread, NULL, dataThread, NULL);
-  pthread_create(&display_thread, NULL, displayThread, NULL);
+  pthread_create(&status_thread, NULL, statusThread, (void*)thread_id);
+  pthread_create(&data_thread, NULL, dataThread, (void*)thread_id);
+  pthread_create(&display_thread, NULL, displayThread, (void*)thread_id);
 
 
   // Clean up at termination
