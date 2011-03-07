8689224604f8d8ae651812cd372b5032615e16cf
Noah Kagan <kagan.noah@gmail.com>
2011-03-06 17:18:17 -0800

    Added makefile

---
+++ qtgui/ui_consolewindow.h
@@ -0,0 +1,101 @@
+/********************************************************************************
+** Form generated from reading UI file 'consolewindow.ui'
+**
+** Created: Sun Mar 6 00:51:55 2011
+**      by: Qt User Interface Compiler version 4.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_CONSOLEWINDOW_H
+#define UI_CONSOLEWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenuBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTextBrowser>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ConsoleWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QTextBrowser *textBrowser;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *clearButton;
+    QPushButton *killButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *ConsoleWindow)
+    {
+        if (ConsoleWindow->objectName().isEmpty())
+            ConsoleWindow->setObjectName(QString::fromUtf8("ConsoleWindow"));
+        ConsoleWindow->resize(394, 307);
+        centralwidget = new QWidget(ConsoleWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+
+        verticalLayout->addWidget(textBrowser);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        clearButton = new QPushButton(centralwidget);
+        clearButton->setObjectName(QString::fromUtf8("clearButton"));
+        clearButton->setMaximumSize(QSize(100, 25));
+
+        horizontalLayout->addWidget(clearButton);
+
+        killButton = new QPushButton(centralwidget);
+        killButton->setObjectName(QString::fromUtf8("killButton"));
+        killButton->setMaximumSize(QSize(100, 25));
+
+        horizontalLayout->addWidget(killButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        ConsoleWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(ConsoleWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 394, 27));
+        ConsoleWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(ConsoleWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        ConsoleWindow->setStatusBar(statusbar);
+
+        retranslateUi(ConsoleWindow);
+
+        QMetaObject::connectSlotsByName(ConsoleWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *ConsoleWindow)
+    {
+        ConsoleWindow->setWindowTitle(QApplication::translate("ConsoleWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        clearButton->setText(QApplication::translate("ConsoleWindow", "Clear", 0, QApplication::UnicodeUTF8));
+        killButton->setText(QApplication::translate("ConsoleWindow", "Kill", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ConsoleWindow: public Ui_ConsoleWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_CONSOLEWINDOW_H
---
+++ qtgui/ui_mainwindow.h
@@ -0,0 +1,185 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created: Sun Mar 6 00:51:55 2011
+**      by: Qt User Interface Compiler version 4.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QGridLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenuBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QRadioButton>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTextEdit>
+#include <QtGui/QToolBar>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QRadioButton *sourceButton;
+    QRadioButton *followerButton;
+    QGridLayout *gridLayout;
+    QLabel *label;
+    QTextEdit *addr;
+    QLabel *label_2;
+    QTextEdit *port;
+    QLabel *label_3;
+    QTextEdit *bw;
+    QLabel *label_4;
+    QTextEdit *media_file;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(220, 280);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        verticalLayout = new QVBoxLayout(centralWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(40);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        sourceButton = new QRadioButton(centralWidget);
+        sourceButton->setObjectName(QString::fromUtf8("sourceButton"));
+        sourceButton->setChecked(true);
+
+        horizontalLayout->addWidget(sourceButton);
+
+        followerButton = new QRadioButton(centralWidget);
+        followerButton->setObjectName(QString::fromUtf8("followerButton"));
+
+        horizontalLayout->addWidget(followerButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setSpacing(6);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setMaximumSize(QSize(16777215, 25));
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        addr = new QTextEdit(centralWidget);
+        addr->setObjectName(QString::fromUtf8("addr"));
+        addr->setMaximumSize(QSize(16777215, 25));
+        addr->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        addr->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+        gridLayout->addWidget(addr, 0, 1, 1, 1);
+
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        gridLayout->addWidget(label_2, 1, 0, 1, 1);
+
+        port = new QTextEdit(centralWidget);
+        port->setObjectName(QString::fromUtf8("port"));
+        port->setMaximumSize(QSize(16777215, 25));
+        port->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        port->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+        gridLayout->addWidget(port, 1, 1, 1, 1);
+
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        gridLayout->addWidget(label_3, 2, 0, 1, 1);
+
+        bw = new QTextEdit(centralWidget);
+        bw->setObjectName(QString::fromUtf8("bw"));
+        bw->setMaximumSize(QSize(16777215, 25));
+        bw->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        bw->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+        gridLayout->addWidget(bw, 2, 1, 1, 1);
+
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        gridLayout->addWidget(label_4, 3, 0, 1, 1);
+
+        media_file = new QTextEdit(centralWidget);
+        media_file->setObjectName(QString::fromUtf8("media_file"));
+        media_file->setMaximumSize(QSize(16777215, 25));
+        media_file->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        media_file->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+        gridLayout->addWidget(media_file, 3, 1, 1, 1);
+
+
+        verticalLayout->addLayout(gridLayout);
+
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setMaximumSize(QSize(100, 25));
+
+        verticalLayout->addWidget(pushButton);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 220, 27));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Eyeunite Tool", 0, QApplication::UnicodeUTF8));
+        sourceButton->setText(QApplication::translate("MainWindow", "Source", 0, QApplication::UnicodeUTF8));
+        followerButton->setText(QApplication::translate("MainWindow", "Follower", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("MainWindow", "IP Address", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("MainWindow", "Listen Port", 0, QApplication::UnicodeUTF8));
+        label_3->setText(QApplication::translate("MainWindow", "Bandwidth", 0, QApplication::UnicodeUTF8));
+        label_4->setText(QApplication::translate("MainWindow", "Media File", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("MainWindow", "Create", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
